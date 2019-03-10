686bda4ff39a503c6a89f7598bf3ca09a943b924
Michael Mottola <mottola64@gmail.com>
2019-03-10 22:49:18 +0000

    Automatically backed up by Learn

---
+++ tweet_shortener.rb
@@ -0,0 +1,13 @@
+def dictionary
+  dictionary = {
+  "too" => "2",
+  "to" => "2",
+  "two" => "2",
+  "four" => "4",
+  "for" => "4",
+  "be" => "b",
+  "you" => "u",
+  "at" => "@",
+  "and" => "&"
+  }
+end
